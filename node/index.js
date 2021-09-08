c5d6ed0067d0bd27b44fd87cc41cdaa62bde683c
Taeyang Kim <mynamesunpower@gmail.com>
2021-09-08 15:08:05 +0900

    파일 경로 수정

--- node/index.js
+++ node/index.js
@@ -1,4 +1,4 @@
-const camelize = require('./array/camelize');
+const camelize = require('./P1_Core_JavaScript/5_Data_Structure/array/camelize');
 
 console.log(camelize('background-color'));
 console.log(camelize('list-style-image'));
